be58e4e63f2804c1a0f6be7952116e973461e130
RDeveLabs <rdevelopamd@gmail.com>
2026-05-25 11:50:33 +0700

    hapus teks text

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const multer = require('multer');
 const app = express();
-test
+
 app.use(cors({
     origin: 'https://test.rdevelabs.com',
     methods: ['POST', 'GET'],
