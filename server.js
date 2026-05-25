f61f94f024dc191d6f09ba512fd9a325fef37d42
RDeveLabs <rdevelopamd@gmail.com>
2026-05-25 11:35:49 +0700

    membuat backend

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const cors = require('cors');
+const multer = require('multer');
+const app = express();
+
+app.use(cors({
+    origin: 'https://test.rdevelabs.com',
+    methods: ['POST', 'GET'],
+    allowedHeaders: ['Content-Type']
+}))
+
+const upload = multer();
+
+app.get('/', (req, res) => {
+    res.send("Haloo");
+})
+
+app.post('/data', upload.none(), (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    res.send("data diterima :" + username + password);
+})
+
+app.listen(3000, () => {
+    console.log("Server aktif");
+})
