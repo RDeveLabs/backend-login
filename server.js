2118d2b45f5e1ca75b780a4779dfd987c2caa37b
RDeveLabs <rdevelopamd@gmail.com>
2026-05-27 07:39:08 +0700

    menambahkan fitur menyimpan ke database

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const cors = require('cors');
 const multer = require('multer');
+const { prisma } = require('./connection');
+const { error } = require('node:console');
+const { use } = require('react');
 const app = express();
 
 app.use(cors({
@@ -15,10 +18,23 @@ app.get('/', (req, res) => {
     res.send("Haloo");
 })
 
-app.post('/data', upload.none(), (req, res) => {
-    const username = req.body.username;
-    const password = req.body.password;
-    res.send("data diterima :" + username + password);
+app.post('/data', upload.none(), async(req, res) => {
+    const userName = req.body.username;
+    const pass = req.body.password;
+    try{
+        const user = await prisma.user.create({
+            data: {
+                username: userName,
+                password: pass
+            }
+        })
+        
+        res.send("berhasil menuliskan ke database" + user);
+    }catch(e){
+        res.send("ada error saat menulis ke database: " + e)
+    }
+
+    res.send("data diterima :" + userName + pass);
 })
 
 app.listen(3000, () => {
